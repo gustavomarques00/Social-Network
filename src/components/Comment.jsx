c8f6df484788579ada4801705f31d8fc45a7ec2f
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-07-02 18:27:45 -0300

    Finalização da parte estática do projeto

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { Trash,ThumbsUp } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/gustavomarques00.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo Marques</strong>
+              <time title="11 de Maio às 8:25h" dateTime="2022-05-11 08:25:00">
+                Cerca de 1 hora atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Dev, parabéns</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -5,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/gustavomarques00.png"
-          />
+          <Avatar src="https://github.com/gustavomarques00.png" />
           <div className={styles.authorInfo}>
             <strong>Meu Nome</strong>
             <span>Meu cargo</span>
@@ -39,6 +38,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
